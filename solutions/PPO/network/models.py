e64f6eba1cff369bd6ecd23d0c997f950565a664
jina <97024838+wikyzhibo@users.noreply.github.com>
2025-12-24 19:15:04 +0800

    ppo算法actor的策略头

---
+++ solutions/PPO/network/models.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch
+
+# --------------- 策略头 ---------------
+class MaskedPolicyHead(nn.Module):
+    def __init__(self, hidden=128,n_obs=None, n_actions=None):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_obs,hidden), nn.ReLU(),
+            nn.Linear(hidden,hidden), nn.ReLU(),
+            nn.Linear(hidden, hidden), nn.ReLU(),
+            nn.Linear(hidden,n_actions)
+        )
+    def forward(self, obs):
+        logits = self.net(obs.to(torch.float32))
+        return logits
